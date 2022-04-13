6444f017c378d8ac1d94e30e2f2cb569c5c0d4b6
HoonT <103490296+HoonT@users.noreply.github.com>
2022-04-13 17:13:02 +0900

    update

---
+++ algorithm/boj/LEVEL3/10950.py
@@ -0,0 +1,17 @@
+# A + B -3
+
+# 내 풀이
+a = int(input())
+#b, c = map(int, input().split())
+t = 1
+
+while t <= a:
+    b, c = map(int, input().split())
+    print(b+c)
+    t = t+1
+    
+# 모범 답안
+# 1.
+import sys
+input()
+s = list(sum(map(int, n.split())) for n in sys.stdin)
---
+++ algorithm/boj/LEVEL3/2739.py
@@ -0,0 +1,6 @@
+# 구구단
+
+i = int(input())
+
+for x in range(1,10):
+    print('{} * {} = {}'.format(i, x, i*x))
