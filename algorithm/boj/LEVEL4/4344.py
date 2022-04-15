ab99238829d9b079e9da0634e6f424840d6563bf
HoonT <103490296+HoonT@users.noreply.github.com>
2022-04-15 19:59:50 +0900

    update

--- algorithm/boj/LEVEL4/4344.py
+++ algorithm/boj/LEVEL4/4344.py
@@ -5,5 +5,36 @@
 '''
 
 # 내 풀이
+a = int(input())
 
+for i in range(a):
+    s = list(map(int, input().split()))
+    
+    b = s[0]
+    c = sum(s[1:])
+    e = round(c / b, 3)
+    f = 0
+    
+    for n in range(1, b+1):
+        if s[n] > e:
+            f += 1
+        g = round((f / b)*100, 3)
+        h = '{:.3f}%'.format(g)
+    print(h)
 # 모범 답안
+import sys
+input = sys.stdin.readline
+
+test_case = int(input())
+
+for _ in range(test_case):
+    data = input().strip().split(' ')
+    scores = list(map(float, data[1:]))
+    average = sum(scores) / len(scores)
+
+    above = 0
+    for score in scores:
+        if score > average:
+            above += 1
+
+    print(f'{(above/len(scores))*100:.3f}%')
