0b26cf93b40b4889086d09400b3beae1ef143e6a
HoonT <103490296+HoonT@users.noreply.github.com>
2022-04-28 21:01:59 +0900

    Update 9020.py

--- algorithm/boj/LEVEL8/9020.py
+++ algorithm/boj/LEVEL8/9020.py
@@ -7,5 +7,28 @@ n의 골드바흐 파티션을 출력하는 프로그램을 작성하시오.
 '''
 
 # 내 풀이
-
-# 모범 답안
+m = 1
+n = 10001
+suso_list = set()
+a = [True for i in range(n)]
+for i in range(2, int(n**0.5)+1):
+    if a[i] == True:
+        for j in range(2*i, n, i):
+            a[j] = False
+for i in range(m,n):
+    if i>1 and a[i] == True:
+        suso_list.add(i)
+        
+T = int(input())
+for k in range(T):
+    k = int(input())
+    dic = {}
+    for i in suso_list: 
+        if k-i in suso_list:        
+            if i <= k-i:                
+                dic[(k-i)-i] = [i, k-i]      
+            else:
+                dic[i-(k-i)] = [k-i, i] 
+            
+    key_list = list(dic.keys())    
+    print(dic[min(key_list)][0],dic[min(key_list)][1])
