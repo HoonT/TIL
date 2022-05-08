08b59e01cc97f16cb2629cb1f3c72db31352d6b9
Hoon-T <wbkhkyg@naver.com>
2022-05-08 10:08:08 +0900

    update

--- test1/test2.py
+++
@@ -1,8 +0,0 @@
-a = int(input())
-b = list(map(int, input().split()))
-
-b.sort()
-if len(b) > 1:
-    print(b[0]*b[len(b)-1])
-elif len(b) == 1:
-    print(b[0]**2)
--- test1/test4.py
+++
@@ -1,5 +0,0 @@
-'''
-itertools — 효율적인 루핑을 위한 이터레이터를 만드는 함수
-    https://docs.python.org/ko/3/library/itertools.html
-
-'''
