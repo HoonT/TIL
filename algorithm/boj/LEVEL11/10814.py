15522bdd860aac3c792b960e6298f3771efce919
Hoon-T <wbkhkyg@naver.com>
2022-05-10 15:37:25 +0900

    Update 10814.py

--- algorithm/boj/LEVEL11/10814.py
+++ algorithm/boj/LEVEL11/10814.py
@@ -1,27 +1,35 @@
 # 문제 : 나이순 정렬
-# 제출일 : 2022.. (:)
+# 제출일 : 2022.05.10 (15:30)
 '''
 이때, 회원들을 나이가 증가하는 순으로, 
 나이가 같으면 먼저 가입한 사람이 앞에 오는 순서로 정렬하는 프로그램을 작성하시오.
 '''
 
 # 내 풀이
-a = int(input())
-b = []
+import sys
+input= sys.stdin.readline
 
-for i in range(a):
-    num, name = input().split()
-    b += [[num, name]]
+N= int(input().rstrip())
+lst=[]
+for i in range(N):
+    lst.append(list(input().split()))
+    lst[i][0] = int(lst[i][0])
 
-c = sorted(b)
-for i in range(a-1):
-    if c[i][0] == c[i+1][0]:
-        if b.index(c[i]) > b.index(c[i+1]):
-            temp = c[i]
-            c[i] = c[i+1]
-            c[i+1] = temp
+lst.sort(key=lambda x:x[0])
 
-for i in range(a):
-    print('{} {}'.format(c[i][0], c[i][1]))
+for i in lst:
+    print(i[0],i[1])
 
 # 모범 답안
+from sys import stdin, stdout
+
+users_by_age = [[] for _ in range(200+1)]
+
+for line in stdin.read().splitlines(True)[1:]:
+    users_by_age[int(line.split()[0])].append(line)
+
+stdout.write(''.join(
+    ''.join(u)
+    for u in
+    users_by_age
+))
