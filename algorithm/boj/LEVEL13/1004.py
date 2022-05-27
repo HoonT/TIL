57518c7913620e95366182e31373fbc20b7211ca
Hoon-T <wbkhkyg@naver.com>
2022-05-27 09:57:09 +0900

    Create 1004.py

---
+++ algorithm/boj/LEVEL13/1004.py
@@ -0,0 +1,28 @@
+# 문제 : 어린 왕자
+# 제출일 : 2022.. (:)
+'''
+은하수 지도, 출발점, 
+도착점이 주어졌을 때 어린 왕자에게 필요한 최소의 
+행성계 진입/이탈 횟수를 구하는 프로그램을 작성해 보자.
+'''
+
+# 내 풀이
+import math
+import sys
+input = sys.stdin.readline
+
+cnt_test = int(input())
+cnt = 0
+for _ in range(cnt_test):
+    x1,y1,x2,y2 = list(map(int, input().split()))
+    cnt_circle = int(input())
+    for _ in range(cnt_circle):
+        c1,c2,r = list(map(int, input().split()))
+        if (x1-c1)**2+(y1-c2)**2 <= r**2:
+            cnt += 1
+        if (x2-c1)**2+(y2-c2)**2 <= r**2:
+            cnt += 1
+    print(cnt)
+    cnt = 0
+
+# 모범 답안
