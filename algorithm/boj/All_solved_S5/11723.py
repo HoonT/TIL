dbccdf9a30f8a5b3d44661e522879038b2646759
HoonT <wbkhkyg@naver.com>
2022-09-20 15:35:34 +0900

    Create 11723.py

---
+++ algorithm/boj/All_solved_S5/11723.py
@@ -0,0 +1,50 @@
+# [TIL][Python] Boj - No.11723 (집합)
+## 제출일 : 2022.09.20 (16:00)
+### 알고리즘 : Implementation / Bitmask
+'''
+비어있는 공집합 S가 주어졌을 때, 아래 연산을 수행하는 프로그램을 작성하시오.
+
+add x: S에 x를 추가한다. (1 ≤ x ≤ 20) S에 x가 이미 있는 경우에는 연산을 무시한다.
+remove x: S에서 x를 제거한다. (1 ≤ x ≤ 20) S에 x가 없는 경우에는 연산을 무시한다.
+check x: S에 x가 있으면 1을, 없으면 0을 출력한다. (1 ≤ x ≤ 20)
+toggle x: S에 x가 있으면 x를 제거하고, 없으면 x를 추가한다. (1 ≤ x ≤ 20)
+all: S를 {1, 2, ..., 20} 으로 바꾼다.
+empty: S를 공집합으로 바꾼다. 
+'''
+
+## 내 풀이
+import sys
+input = sys.stdin.readline
+
+m = int(input())
+S = set()
+
+for _ in range(m):
+    temp = input().split()
+    
+    if len(temp) == 1:
+        if temp[0] == "all":
+            S = set([i for i in range(1, 21)])
+        else:
+            S = set()
+    
+    else:
+        func, x = temp[0], temp[1]
+        x = int(x)
+
+        if func == "add":
+            S.add(x)
+        elif func == "remove":
+            S.discard(x)
+        elif func == "check":
+            if x in S:
+                print(1)
+            else:
+                print(0)
+        elif func == "toggle":
+            if x in S:
+                S.discard(x)
+            else:
+                S.add(x)
+
+## 피드백
